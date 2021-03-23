6936f52464700935be24266bc212555eb01f3348
Dunateo <bruvalentin84@icloud.com>
2021-03-23 19:39:57 +0100

    main appel parsedoc

--- src/main/java/Parsedoc.java
+++ src/main/java/Parsedoc.java
@@ -1,16 +1,17 @@
 public class Parsedoc {
 
-    public Code2d parseDdoc(String docres) {
-
-
+    public static Code2d parseDdoc(String docres) {
+        Code2d res = null;
         String version = docres.substring(3, 4);
-        switch (version) {
-            case 01:
+
+        switch (Integer.parseInt(version)) {
+            case 1:
                 for (int i = 4; i < 8; i++)
                     System.out.println();
                 break;
-            case 02:
+            case 2:
                 // code block
+                //res = new Code2d();
                 break;
             case 3:
                 // code block
@@ -20,6 +21,6 @@ public class Parsedoc {
                 break;
         }
 
-
+        return res;
     }
 }
